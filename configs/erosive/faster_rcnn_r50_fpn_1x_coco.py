7f6080dddf96857c3279c2208f27b40f72888e62
qilei123 <qileimail123@gmail.com>
2021-03-14 20:29:36 -0400

    train faster-rcnn on erosive

--- configs/erosive/faster_rcnn_r50_fpn_1x_coco.py
+++ configs/erosive/faster_rcnn_r50_fpn_1x_coco.py
@@ -2,23 +2,26 @@ _base_ = '../faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 
 # 1. dataset settings
 dataset_type = 'CocoDataset'
-#classes = ('erosive')
+classes = ('erosive')
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         # explicitly add your class names to the field `classes`
+        classes=classes,
         ann_file='/data1/qilei_chen/DATA/erosive/annotations/train.json',
         img_prefix='/data1/qilei_chen/DATA/erosive/images'),
     val=dict(
         type=dataset_type,
         # explicitly add your class names to the field `classes`
+        classes=classes,
         ann_file='/data1/qilei_chen/DATA/erosive/annotations/test.json',
         img_prefix='/data1/qilei_chen/DATA/erosive/images/'),
     test=dict(
         type=dataset_type,
         # explicitly add your class names to the field `classes`
+        classes=classes,
         ann_file='/data1/qilei_chen/DATA/erosive/annotations/test.json',
         img_prefix='/data1/qilei_chen/DATA/erosive/images/'))
 

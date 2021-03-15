f6acf7d65c73e78abcc1eadffca419eef5b9730a
qilei123 <qileimail123@gmail.com>
2021-03-14 20:12:36 -0400

    train faster-rcnn on erosive

--- train_on_multi_gpu.sh
+++ train_on_multi_gpu.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 PORT=29500 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 PORT=13546 \
 ./tools/dist_train.sh \
     configs/erosive/faster_rcnn_r50_fpn_1x_coco.py 4 \
     '--work-dir /data1/qilei_chen/DATA/erosive/work_dirs/faster_rcnn_r50_fpn_1x_coco/'

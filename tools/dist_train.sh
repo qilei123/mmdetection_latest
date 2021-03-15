e73673ea90d3f0b6af1c9dbcf6e8d7f2eaf3b551
qilei123 <qileimail123@gmail.com>
2021-03-14 20:07:02 -0400

    train faster-rcnn on erosive

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -3,7 +3,7 @@
 CONFIG=$1
 GPUS=$2
 PORT=${PORT:-29500}
-WORK_DIR=$3
+
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3} --work-dir $WORK_DIR
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}

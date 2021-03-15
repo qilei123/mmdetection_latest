14aa2ea72de85a40efe42e236f9c6814bff0f97d
qilei123 <qileimail123@gmail.com>
2021-03-15 14:23:24 -0400

    commit

---
+++ configs/erosive/cascade_rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,87 @@
+_base_ = '../cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco.py'
+# 1. dataset settings
+classes = ('erosive',)
+data_root = '/data1/qilei_chen/DATA/erosive/'
+data = dict(
+    train=dict(
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file= data_root+'annotations/train.json',
+        img_prefix=data_root+'images/'),
+    val=dict(
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file= data_root+'annotations/test.json',
+        img_prefix=data_root+'images/'),
+    test=dict(
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file= data_root+'annotations/test.json',
+        img_prefix=data_root+'images/'),)
+
+# 2. model settings
+
+# explicitly over-write all the `num_classes` field from default 80 to 1.
+model = dict(
+    roi_head=dict(
+        type='CascadeRoIHead',
+        num_stages=3,
+        stage_loss_weights=[1, 0.5, 0.25],
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=[
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=1,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=1,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.05, 0.05, 0.1, 0.1]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=1,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.033, 0.033, 0.067, 0.067]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
+        ]),
+)# explicitly over-write all the `num_classes` field from default 80 to 1.
--- train_on_multi_gpu.sh
+++ train_on_multi_gpu.sh
@@ -3,7 +3,22 @@
 #     configs/erosive/faster_rcnn_r50_fpn_1x_coco.py 4 \
 #     '--work-dir /data1/qilei_chen/DATA/erosive/work_dirs/faster_rcnn_r50_fpn_1x_coco/'
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 PORT=13646 \
+# CUDA_VISIBLE_DEVICES=0,1,2,3 PORT=13646 \
+# ./tools/dist_train.sh \
+#     configs/erosive/retinanet_r50_fpn_1x_coco.py 4 \
+#     '--work-dir /data1/qilei_chen/DATA/erosive/work_dirs/retinanet_r50_fpn_1x_coco/'
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 PORT=14646 \
 ./tools/dist_train.sh \
-    configs/erosive/retinanet_r50_fpn_1x_coco.py 4 \
-    '--work-dir /data1/qilei_chen/DATA/erosive/work_dirs/retinanet_r50_fpn_1x_coco/'
+    configs/erosive/detr_r50_8x2_150e_coco.py 4 \
+    '--work-dir /data1/qilei_chen/DATA/erosive/work_dirs/detr_r50_8x2_150e_coco/'
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 PORT=12646 \
+./tools/dist_train.sh \
+    configs/erosive/reppoints_moment_r50_fpn_1x_coco.py 4 \
+    '--work-dir /data1/qilei_chen/DATA/erosive/work_dirs/reppoints_moment_r50_fpn_1x_coco/'
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 PORT=9646 \
+./tools/dist_train.sh \
+    configs/erosive/cascade_rcnn_r50_fpn_1x_coco.py 4 \
+    '--work-dir /data1/qilei_chen/DATA/erosive/work_dirs/cascade_rcnn_r50_fpn_1x_coco/'

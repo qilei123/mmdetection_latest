ddbdf0c95603775b4dc3a925f6bf606b2c6bded7
qilei123 <qileimail123@gmail.com>
2021-03-15 21:06:47 -0400

    commit

--- inference.py
+++ inference.py
@@ -29,6 +29,7 @@ for key in coco_imgs:
         cv2.rectangle(img, (int(x), int(y)), (int(x+w), int(y+h)), (0,255,0), 2)
 
     result = inference_detector(model, img)
-    model.show_result(img, result,score_thr=0.1,bbox_color =(255,0,0),
+    score_thr=0.1
+    model.show_result(img, result,score_thr=score_thr,bbox_color =(255,0,0),
                     text_color = (255,0,0),font_size=5, 
-                    out_file='/data1/qilei_chen/DATA/erosive/work_dirs/faster_rcnn_r50_fpn_1x_coco/test_result/'+img_file_name)
+                    out_file='/data1/qilei_chen/DATA/erosive/work_dirs/faster_rcnn_r50_fpn_1x_coco/test_result_'+str(score_thr)+'/'+img_file_name)

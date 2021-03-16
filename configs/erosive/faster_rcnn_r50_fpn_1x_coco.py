abd4e9bb1a6b99ac3e01ab28f5ead595d81fdc70
qilei123 <qileimail123@gmail.com>
2021-03-16 01:50:48 -0400

    commit

--- configs/erosive/faster_rcnn_r50_fpn_1x_coco.py
+++ configs/erosive/faster_rcnn_r50_fpn_1x_coco.py
@@ -33,7 +33,17 @@ model = dict(
     roi_head=dict(
         bbox_head=
             dict(type='Shared2FCBBoxHead',
-                num_classes=1)))# explicitly over-write all the `num_classes` field from default 80 to 1.
+                num_classes=1)),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=1000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.5),
+            min_bbox_size=0),
+        rcnn=dict(
+            score_thr=0.05,
+            nms=dict(type='nms', iou_threshold=0.1),
+            max_per_img=100)))# explicitly over-write all the `num_classes` field from default 80 to 1.
 
 runner = dict(type='EpochBasedRunner', max_epochs=24)
 resume_from = "/data1/qilei_chen/DATA/erosive/work_dirs/faster_rcnn_r50_fpn_1x_coco/latest.pth"

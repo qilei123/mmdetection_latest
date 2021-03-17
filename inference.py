38587ff4ec75a5a50a8d819ff5891f5136a1008e
qilei123 <qileimail123@gmail.com>
2021-03-17 09:29:28 -0400

    commit

--- inference.py
+++ inference.py
@@ -4,8 +4,8 @@ from pycocotools.coco import COCO
 import os
 import cv2
 # Specify the path to model config and checkpoint file
-config_file = 'configs/erosive/faster_rcnn_r50_fpn_1x_coco.py'
-checkpoint_file = '/data1/qilei_chen/DATA/erosive/work_dirs/faster_rcnn_r50_fpn_1x_coco/epoch_10.pth'
+config_file = 'configs/erosive/reppoints_moment_r50_fpn_1x_coco.py'
+checkpoint_file = '/data1/qilei_chen/DATA/erosive/work_dirs/reppoints_moment_r50_fpn_1x_coco/epoch_83.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
@@ -33,4 +33,4 @@ for key in coco_imgs:
     score_thr=0.1
     model.show_result(img, result,score_thr=score_thr,bbox_color =(255,0,0),
                     text_color = (255,0,0),font_size=5, 
-                    out_file='/data1/qilei_chen/DATA/erosive/work_dirs/faster_rcnn_r50_fpn_1x_coco/'+set_name+'_result_'+str(score_thr)+'/'+img_file_name)
+                    out_file='/data1/qilei_chen/DATA/erosive/work_dirs/reppoints_moment_r50_fpn_1x_coco/'+set_name+'_result_'+str(score_thr)+'/'+img_file_name)

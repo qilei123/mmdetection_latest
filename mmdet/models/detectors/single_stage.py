f7c228654423e4722c2521f304bc9200394ae18e
qilei123 <qileimail123@gmail.com>
2021-03-18 16:25:09 -0400

    commit

--- mmdet/models/detectors/single_stage.py
+++ mmdet/models/detectors/single_stage.py
@@ -109,15 +109,19 @@ class SingleStageDetector(BaseDetector):
                 The outer list corresponds to each image. The inner list
                 corresponds to each class.
         """
+        
         x = self.extract_feat(img)
+        
         outs = self.bbox_head(x)
         # get origin input shape to support onnx dynamic shape
         if torch.onnx.is_in_onnx_export():
             # get shape as tensor
             img_shape = torch._shape_as_tensor(img)[2:]
             img_metas[0]['img_shape_for_onnx'] = img_shape
+        
         bbox_list = self.bbox_head.get_bboxes(
             *outs, img_metas, rescale=rescale)
+        
         # skip post-processing when exporting to ONNX
         if torch.onnx.is_in_onnx_export():
             return bbox_list
@@ -126,6 +130,7 @@ class SingleStageDetector(BaseDetector):
             bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
             for det_bboxes, det_labels in bbox_list
         ]
+
         return bbox_results
 
     def aug_test(self, imgs, img_metas, rescale=False):

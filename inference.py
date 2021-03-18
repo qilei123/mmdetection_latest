4cfd21054d86bc224bf1eb4af4b143d333260252
qilei123 <qileimail123@gmail.com>
2021-03-17 20:13:07 -0400

    commit

--- inference.py
+++ inference.py
@@ -4,8 +4,11 @@ from pycocotools.coco import COCO
 import os
 import cv2
 # Specify the path to model config and checkpoint file
-config_file = 'configs/erosive/reppoints_moment_r50_fpn_1x_coco.py'
-checkpoint_file = '/data1/qilei_chen/DATA/erosive/work_dirs/reppoints_moment_r50_fpn_1x_coco/epoch_83.pth'
+model_name = 'reppoints_moment_r50_fpn_1x_coco'
+score_thr=0.25
+
+config_file = 'configs/erosive/'+model_name+'.py'
+checkpoint_file = '/data1/qilei_chen/DATA/erosive/work_dirs/'+model_name+'/epoch_83.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
@@ -31,7 +34,7 @@ for key in coco_imgs:
     for ann in anns:
         [x,y,w,h] = ann['bbox']
         cv2.rectangle(img, (int(x), int(y)), (int(x+w), int(y+h)), (0,255,0), 2)
-    score_thr=0.2
+    
     model.show_result(img, result,score_thr=score_thr,bbox_color =(255,0,0),
                     text_color = (255,0,0),font_size=5, 
-                    out_file='/data1/qilei_chen/DATA/erosive/work_dirs/reppoints_moment_r50_fpn_1x_coco/'+set_name+'_result_'+str(score_thr)+'/'+img_file_name)
+                    out_file='/data1/qilei_chen/DATA/erosive/work_dirs/'+model_name+'/'+set_name+'_result_'+str(score_thr)+'/'+img_file_name)

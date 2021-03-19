0e1266d7e1a2be9a6fd7e6a89447ffdb99f6df95
qilei123 <qileimail123@gmail.com>
2021-03-19 18:05:25 -0400

    commit

--- mmdet/models/utils/__init__.py
+++ mmdet/models/utils/__init__.py
@@ -6,11 +6,13 @@ from .res_layer import ResLayer, SimplifiedBasicBlock
 from .transformer import (FFN, DynamicConv, MultiheadAttention, Transformer,
                           TransformerDecoder, TransformerDecoderLayer,
                           TransformerEncoder, TransformerEncoderLayer)
-
+from .inverted_residual import InvertedResidual, InvertedResidualV3
+from .make_divisible import make_divisible
 __all__ = [
     'ResLayer', 'gaussian_radius', 'gen_gaussian_target', 'MultiheadAttention',
     'FFN', 'TransformerEncoderLayer', 'TransformerEncoder',
     'TransformerDecoderLayer', 'TransformerDecoder', 'Transformer',
     'build_transformer', 'build_positional_encoding', 'SinePositionalEncoding',
-    'LearnedPositionalEncoding', 'DynamicConv', 'SimplifiedBasicBlock'
+    'LearnedPositionalEncoding', 'DynamicConv', 'SimplifiedBasicBlock',
+    'make_divisible', 'InvertedResidual'
 ]

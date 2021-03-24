1ee98889961fea5bb90278eda6a57c443d3bf2e0
qilei123 <qileimail123@gmail.com>
2021-03-24 09:23:40 -0400

    commit

--- inference.py
+++ inference.py
@@ -228,12 +228,12 @@ if __name__=="__main__":
     coco_instance = COCO(anns_file)
     
     
-    model_name = 'faster_rcnn_r50_fpn_1x_coco_with_empty'
+    model_name = 'faster_rcnn_mobilev2_fpn_1x_coco'
     work_dir = '/data1/qilei_chen/DATA/erosive/work_dirs/'
     model_epoch = 'epoch_10.pth'
     
     results_file_dir = os.path.join(work_dir,model_name,model_epoch+"_"+set_name+".pkl")
-    #results_file_dir = generate_result(model_name,work_dir,model_epoch,coco_instance,set_name,imshow=True)
+    results_file_dir = generate_result(model_name,work_dir,model_epoch,coco_instance,set_name,imshow=True)
 
     peval(results_file_dir,coco_instance,thresh=0.3,with_empty_images=True)
     

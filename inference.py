c406c01eca5107c8d810cce57af682216886bb77
qilei123 <qileimail123@gmail.com>
2021-04-06 23:44:25 -0400

    commit

--- inference.py
+++ inference.py
@@ -96,7 +96,7 @@ def inference_and_save_result(model,coco_instance,img_folder_dir,
                 cv2.rectangle(img, (int(x), int(y)), (int(x+w), int(y+h)), colors[ann['category_id']], 2)
             out_file = result_save_dir+'_result_'+str(score_thr)+'/'+img_file_name
             model.show_result(img, result,score_thr=score_thr,bbox_color =colors[2],
-                            text_color = (255,0,0),font_size=10, 
+                            text_color = colors[2],font_size=10, 
                             out_file=out_file)            
 
         results[coco_imgs[key]['id']] = dict()

91dad19b4c1e5fd9b5a97445ef674b4ca8d1e56e
qilei123 <qileimail123@gmail.com>
2021-04-13 14:58:50 -0400

    commit

--- inference.py
+++ inference.py
@@ -372,11 +372,11 @@ def test_video():
     fps = src_cap.get(cv2.CAP_PROP_FPS)
     frame_size = (int(src_cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2),
                   int(src_cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    if not os.path.exists('/data1/qilei_chen/DATA/'+category+'/video_test_results/'+model_name+"_l"):
-        os.makedirs('/data1/qilei_chen/DATA/'+category+'/video_test_results/'+model_name+"_l")
-    save_dir = os.path.join('/data1/qilei_chen/DATA/'+category+'/video_test_results/',model_name+"_l", os.path.basename(video_dir))
-    dst_writer = cv2.VideoWriter(save_dir, cv2.VideoWriter_fourcc(
-        "P", "I", "M", "1"), fps, frame_size)
+    if not os.path.exists('/data1/qilei_chen/DATA/'+category+'/video_test_results/'+model_name):
+        os.makedirs('/data1/qilei_chen/DATA/'+category+'/video_test_results/'+model_name)
+    
+    save_dir = os.path.join('/data1/qilei_chen/DATA/'+category+'/video_test_results/',model_name, os.path.basename(video_dir))
+    dst_writer = cv2.VideoWriter(save_dir, cv2.VideoWriter_fourcc("P", "I", "M", "1"), fps, frame_size)
     
     positive_records = open(save_dir+".txt","w")
 
@@ -393,7 +393,7 @@ def test_video():
                             text_color=colors[2], font_size=10)
         
         cv2.putText(frame,str(count),(30,30),cv2.FONT_HERSHEY_SIMPLEX, 1,colors[2],1,cv2.LINE_AA)
-        #cv2.imwrite("/data1/qilei_chen/DATA/ulcer/video_test_results/test.jpg",frame)
+        cv2.imwrite("/data1/qilei_chen/DATA/ulcer/video_test_results/test.jpg",frame)
         
         box_count=0
         #print(len(result[0]))
@@ -403,7 +403,9 @@ def test_video():
 
         #print(box_count)
         dst_writer.write(frame)
-        positive_records.write(str(count)+" "+str(box_count)+" "+str(box_count!=0)+"\n")        
+        print(str(count)+" "+str(box_count)+" "+str(box_count!=0)+"\n")
+        positive_records.write(str(count)+" "+str(box_count)+" "+str(box_count!=0)+"\n")   
+
         count +=1
         success, frame = src_cap.read()
 

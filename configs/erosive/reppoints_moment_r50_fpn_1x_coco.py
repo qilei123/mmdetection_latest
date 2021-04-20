386090448d9d26b2196f54d48278a06afb983926
qilei123 <qileimail123@gmail.com>
2021-04-20 02:22:17 -0400

    commit

--- configs/erosive/reppoints_moment_r50_fpn_1x_coco.py
+++ configs/erosive/reppoints_moment_r50_fpn_1x_coco.py
@@ -32,5 +32,5 @@ model = dict(
         nms=dict(type='nms', iou_threshold=0.01),
         max_per_img=100))# explicitly over-write all the `num_classes` field from default 80 to 1.
 
-runner = dict(type='EpochBasedRunner', max_epochs=24)
-#resume_from = "/data1/qilei_chen/DATA/erosive/work_dirs/reppoints_moment_r50_fpn_1x_coco/latest.pth"
+runner = dict(type='EpochBasedRunner', max_epochs=64)
+resume_from = "/data1/qilei_chen/DATA/erosive/work_dirs/reppoints_moment_r50_fpn_1x_coco_4_19/latest.pth"

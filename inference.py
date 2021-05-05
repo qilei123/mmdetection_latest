3b728e84f84d512c629ec33549e64a1062cdd223
qilei123 <qileimail123@gmail.com>
2021-05-05 00:12:51 -0400

    commit

--- inference.py
+++ inference.py
@@ -447,8 +447,8 @@ def test_images(model_name = 'cascade_rcnn_r50_fpn_1x_coco_fine',model_epoch = '
 
     results_file_dir = os.path.join(
         work_dir, model_name, model_epoch+"_"+set_name+".pkl")
-    results_file_dir = generate_result(
-        model_name, work_dir, model_epoch, coco_instance,data_set_name = 'erosive', set_name = set_name, imshow=True)
+    #results_file_dir = generate_result(
+    #    model_name, work_dir, model_epoch, coco_instance,data_set_name = 'erosive', set_name = set_name, imshow=True)
     for thresh in range(0,100,5):
         thresh = float(thresh)/100
         print('------------threshold:'+str(thresh)+'--------------')
@@ -672,4 +672,4 @@ if __name__ == "__main__":
     test_images(model_name = 'faster_rcnn_mobilev2_fpn_1x_coco_fine',model_epoch = 'epoch_22.pth')
     test_images(model_name = 'faster_rcnn_r50_fpn_1x_coco_fine',model_epoch = 'epoch_9.pth')
     '''
-    test_video_batch()
+    #test_video_batch()

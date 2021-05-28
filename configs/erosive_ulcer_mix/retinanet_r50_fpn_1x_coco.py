857b74555928800b326a43f27926723ac690cf4a
qilei123 <qileimail123@gmail.com>
2021-05-28 19:53:19 -0400

    commit

--- configs/erosive_ulcer_mix/retinanet_r50_fpn_1x_coco.py
+++ configs/erosive_ulcer_mix/retinanet_r50_fpn_1x_coco.py
@@ -28,6 +28,6 @@ data = dict(
 # explicitly over-write all the `num_classes` field from default 80 to 1.
 model = dict(
     bbox_head=
-        dict(num_classes=1))# explicitly over-write all the `num_classes` field from default 80 to 1.
+        dict(num_classes=2))# explicitly over-write all the `num_classes` field from default 80 to 1.
 runner = dict(type='EpochBasedRunner', max_epochs=24)
 #resume_from = "/data1/qilei_chen/DATA/erosive/work_dirs/retinanet_r50_fpn_1x_coco_fine/latest.pth"

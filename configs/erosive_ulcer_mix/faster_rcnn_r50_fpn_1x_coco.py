57c2f1577d2c2d214ed286eb39f6584a47340245
qilei123 <qileimail123@gmail.com>
2021-05-28 20:06:01 -0400

    commit

---
+++ configs/erosive_ulcer_mix/faster_rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,50 @@
+_base_ = '../faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+
+# 1. dataset settings
+dataset_type = 'CocoDataset'
+classes = ('ulcer','erosive')
+num_classes=2
+data_root = '/data1/qilei_chen/DATA/erosive_ulcer_mix/'
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file= data_root+'annotations/train_mix.json',
+        img_prefix=data_root+'images/'),
+    val=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file= data_root+'annotations/test_mix.json',
+        img_prefix=data_root+'images/'),
+    test=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file= data_root+'annotations/test_mix.json',
+        img_prefix=data_root+'images/'),)
+
+# 2. model settings
+
+# explicitly over-write all the `num_classes` field from default 80 to 1.
+model = dict(
+    roi_head=dict(
+        bbox_head=
+            dict(type='Shared2FCBBoxHead',
+                num_classes=num_classes)),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=1000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.5),
+            min_bbox_size=0),
+        rcnn=dict(
+            score_thr=0.05,
+            nms=dict(type='nms', iou_threshold=0.1),
+            max_per_img=100)))# explicitly over-write all the `num_classes` field from default 80 to 1.
+
+runner = dict(type='EpochBasedRunner', max_epochs=12)
+#resume_from = "/data1/qilei_chen/DATA/erosive/work_dirs/faster_rcnn_r50_fpn_1x_coco_with_empty/latest.pth"

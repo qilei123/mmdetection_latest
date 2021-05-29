b08bc36200126a28b60dc6856019851add27c592
qilei123 <qileimail123@gmail.com>
2021-05-29 01:33:12 -0400

    commit

--- configs/erosive_ulcer_mix/faster_rcnn_r50_fpn_1x_coco.py
+++ configs/erosive_ulcer_mix/faster_rcnn_r50_fpn_1x_coco.py
@@ -2,7 +2,7 @@ _base_ = '../faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 
 # 1. dataset settings
 dataset_type = 'CocoDataset'
-classes = ('ulcer','erosive')
+classes = ('erosive','ulcer')
 num_classes=2
 data_root = '/data1/qilei_chen/DATA/erosive_ulcer_mix/'
 data = dict(

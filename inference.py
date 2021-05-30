b07cfbf71f795ce8d62ded0192efd47a5d092e6c
qilei123 <qileimail123@gmail.com>
2021-05-30 16:11:54 -0400

    commit

--- inference.py
+++ inference.py
@@ -703,4 +703,4 @@ if __name__ == "__main__":
     test_images(model_name = 'faster_rcnn_mobilev2_fpn_1x_coco_fine',model_epoch = 'epoch_22.pth')
     test_images(model_name = 'faster_rcnn_r50_fpn_1x_coco_fine',model_epoch = 'epoch_9.pth')
     '''
-    test_video_batch()
+    test_video_batch(16)

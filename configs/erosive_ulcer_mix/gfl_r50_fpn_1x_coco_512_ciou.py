4fb255b4b7d97fd4c1eebe37fc149c1b00725eec
qilei123 <qileimail123@gmail.com>
2021-06-09 18:22:11 -0400

    commit

--- configs/erosive_ulcer_mix/gfl_r50_fpn_1x_coco_512_ciou.py
+++ configs/erosive_ulcer_mix/gfl_r50_fpn_1x_coco_512_ciou.py
@@ -111,14 +111,14 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=0.001,
     step=[8, 11])
-runner = dict(type='EpochBasedRunner', max_epochs=12)
+runner = dict(type='EpochBasedRunner', max_epochs=24)
 checkpoint_config = dict(interval=1)
 log_config = dict(interval=50, hooks=[dict(type='TextLoggerHook')])
 custom_hooks = [dict(type='NumClassCheckHook')]
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 load_from = None
-resume_from = None
+resume_from = data_root+"work_dirs/gfl_r50_fpn_1x_coco_512_ciou/latest.pth"
 workflow = [('train', 1)]
 model = dict(
     type='GFL',

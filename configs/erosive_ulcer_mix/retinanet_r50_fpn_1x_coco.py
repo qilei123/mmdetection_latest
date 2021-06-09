0e6b88c38e945aaca424bb8b91d140f7de6784a3
qilei123 <qileimail123@gmail.com>
2021-06-09 19:06:02 -0400

    commit

--- configs/erosive_ulcer_mix/retinanet_r50_fpn_1x_coco.py
+++ configs/erosive_ulcer_mix/retinanet_r50_fpn_1x_coco.py
@@ -29,6 +29,12 @@ data = dict(
 optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 model = dict(
     bbox_head=
-        dict(num_classes=2))# explicitly over-write all the `num_classes` field from default 80 to 1.
+        dict(num_classes=2),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='nms', iou_threshold=0.1),
+        max_per_img=100))# explicitly over-write all the `num_classes` field from default 80 to 1.
 runner = dict(type='EpochBasedRunner', max_epochs=24)
 #resume_from = "/data1/qilei_chen/DATA/erosive_ulcer_mix/work_dirs/retinanet_r50_fpn_1x_coco/latest.pth"
--- configs/erosive_ulcer_mix/retinanet_r50_fpn_1x_coco_512.py
+++ configs/erosive_ulcer_mix/retinanet_r50_fpn_1x_coco_512.py
@@ -56,7 +56,7 @@ model = dict(
         nms_pre=1000,
         min_bbox_size=0,
         score_thr=0.05,
-        nms=dict(type='nms', iou_threshold=0.5),
+        nms=dict(type='nms', iou_threshold=0.1),
         max_per_img=100))
 dataset_type = 'CocoDataset'
 data_root = '/data1/qilei_chen/DATA/erosive_ulcer_mix/'

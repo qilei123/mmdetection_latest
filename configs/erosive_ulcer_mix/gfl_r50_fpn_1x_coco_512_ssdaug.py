f38e25b89ae2970c662dcfce39987573c1e941fc
qilei123 <qileimail123@gmail.com>
2021-06-10 00:26:11 -0400

    commit

--- configs/erosive_ulcer_mix/gfl_r50_fpn_1x_coco_512_ssdaug.py
+++ configs/erosive_ulcer_mix/gfl_r50_fpn_1x_coco_512_ssdaug.py
@@ -118,7 +118,8 @@ data = dict(
                 ])
         ]),classes=('erosive', 'ulcer'))
 evaluation = dict(interval=1, metric='bbox')
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+#optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='Adam', lr=0.0005)
 optimizer_config = dict(grad_clip=None)
 lr_config = dict(
     policy='step',

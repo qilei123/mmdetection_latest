122b4ec117009107cf93628304e2c061287a3c3d
qilei123 <qileimail123@gmail.com>
2021-09-16 13:38:46 -0400

    commit

--- mmdet/datasets/polyp_dataset.py
+++ mmdet/datasets/polyp_dataset.py
@@ -26,7 +26,7 @@ class PolypDataset(CustomDataset):
 
     #CLASSES = ('polyp', 'instrument')
     #cat_ids = [1,2]
-    CLASSES = ('polyp')
+    CLASSES = ('polyp',)
     cat_ids = [1]
     def __init__(self, clean_ann=False,empty_ann=1, **kwargs):
         self.clean_ann = clean_ann 

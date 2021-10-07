39dfd45c2e1abdf7f98c0940d72689bab2264a0e
qilei123 <qileimail123@gmail.com>
2021-10-07 19:32:57 -0400

    commit

--- configs/_trans_drone/mask_rcnn_r50_fpn_2x_coco_wide.py
+++ configs/_trans_drone/mask_rcnn_r50_fpn_2x_coco_wide.py
@@ -117,11 +117,11 @@ model = dict(
         rpn=dict(
             nms_pre=1000,
             max_per_img=1000,
-            nms=dict(type='nms', iou_threshold=0.7),
+            nms=dict(type='nms', iou_threshold=0.1),
             min_bbox_size=0),
         rcnn=dict(
             score_thr=0.05,
-            nms=dict(type='nms', iou_threshold=0.5),
+            nms=dict(type='nms', iou_threshold=0.1),
             max_per_img=100,
             mask_thr_binary=0.5)))
 dataset_type = 'CocoDataset'
